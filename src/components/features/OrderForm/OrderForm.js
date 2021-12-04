65263a31a7a836af791b6d395eda4f2a98cb81bb
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-12-04 20:59:37 +0100

    Add 18.2 TASK: Travel Agency - Trip Cost - code improvement v2

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,13 +3,15 @@ import OrderSummary from '../OrderSummary/OrderSummary';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const OrderForm = ({ tripCost, options }) => {
-  <Grid>
-    <Row>
-      <Col xs={12}>
-        <OrderSummary tripCost={tripCost} tripOptions={options} />
-      </Col>
-    </Row>
-  </Grid>
+  return (
+    <Grid>
+      <Row>
+        <Col xs={12}>
+          <OrderSummary tripCost={tripCost} tripOptions={options} />
+        </Col>
+      </Row>
+    </Grid>
+  );
 };
 
 export default OrderForm;
