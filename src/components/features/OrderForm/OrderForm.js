063de4b0e23e2741c6c81f182fe8d82dd55079bb
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-12-05 17:22:19 +0100

    Add 18.3 Travel Agency - Displaying single option - in progress

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
-const OrderForm = ({ tripCost, options }) => {
+const OrderForm = ({ tripCost, options, pricing, id}) => {
   return (
     <Grid>
       <Row>
+        {pricing.map(options => (
+          <Col md={4} key={id}>
+            <OrderOption {...options} />
+          </Col>
+        ))}
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} tripOptions={options} />
         </Col>
       </Row>
-    </Grid>
+    </Grid >
   );
 };
 
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './OrderOption.module.scss';
+
+const OrderOption = () => {
+  return (
+    <div>
+      <h3 className={styles.title}>Car Rental</h3>
+    </div>
+  );
+};
+
+export default OrderOption;
