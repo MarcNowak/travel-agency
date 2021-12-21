4ff417692ad5b7e81398901646e5c70e4156a0a8
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-12-21 21:31:57 +0100

    Add 20.4 Travel Agency - TDD - Option 3 - Various Phone Numbers v3

--- src/components/layout/Phone/Phone.test.js
+++ src/components/layout/Phone/Phone.test.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Phone from './Phone';
-
-const select = {
-  name: '.name',
-};
-
-const mockProps = {
-  name: '8:00 - 12:00 - Amanda, 678.243.8455',
-};
+import Icon from '../../common/Icon/Icon';
 
 describe('Component Phone', () => {
 
@@ -19,16 +12,65 @@ describe('Component Phone', () => {
 
   });
 
-  it('TEST 101: should contain name', () => {
+  it('TEST 101: should contain span and icon', () => {
     const component = shallow(<Phone />);
-    expect(component.exists(select.name)).toEqual(true);
+    expect(component.exists('span')).toEqual(true);
+    expect(component.exists(Icon)).toEqual(true);
 
   });
 
-  it('TEST 102: should contain correct name', () => {
-    const component = shallow(<Phone {...mockProps} />);
+  const trueDate = Date;
+  const mockDate = customDate => class extends Date {
+    constructor(...args) {
+      if (args.length) {
+        super(...args);
+      } else {
+        super(customDate);
+      }
+      return this;
+    }
+    static now() {
+      return (new Date(customDate)).getTime();
+    }
+  };
 
-    expect(component.find(select.name).text()).toEqual(mockProps.name);
+
+  it(`TEST 102: should show Amanda phone number beetween 8-12 UTC`, () => {
+    global.Date = mockDate(`2019-05-14T10:00:01.135Z`);
+
+    const component = shallow(<Phone />);
+    expect(component.find('span').text()).toEqual('Amanda, 678.243.8455');
+
+    global.Date = trueDate;
   });
+
+  it(`TEST 103: should show Tobias phone number beetween 12-16 UTC`, () => {
+    global.Date = mockDate(`2019-05-14T14:00:01.135Z`);
+
+    const component = shallow(<Phone />);
+    expect(component.find('span').text()).toEqual('Tobias, 278.443.6443');
+
+    global.Date = trueDate;
+  });
+
+  it(`TEST 104: should show Tobias phone number beetween 16-22 UTC`, () => {
+    global.Date = mockDate(`2019-05-14T18:00:01.135Z`);
+
+    const component = shallow(<Phone />);
+    expect(component.find('span').text()).toEqual('Helena, 167.280.3970');
+
+    global.Date = trueDate;
+  });
+
+  it(`TEST 105: should show Office is closed message`, () => {
+    global.Date = mockDate(`2019-05-14T03:00:01.135Z`);
+
+    const component = shallow(<Phone />);
+    expect(component.find('span').text()).toEqual('The office opens at 8:00 UTC');
+
+    global.Date = trueDate;
+  });
+
   
+
 });
